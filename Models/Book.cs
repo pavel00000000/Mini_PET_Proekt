851f9fd9ba7e7a928bb9f51eec8544de851bf5e3
roboty.p.d <freddik1990@gmail.com>
2024-11-10 20:33:17 +0200

    Add Book model to represent book entities

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Mini_PET_Proekt.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+}
