24c0229fc61065bf2224270cf3a64bc8824f0bde
roboty.p.d <freddik1990@gmail.com>
2024-11-10 20:41:31 +0200

    Configure DI and Swagger in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Mini_PET_Proekt.Services;
+using MySql.Data.MySqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Регистрация зависимостей
+builder.Services.AddSingleton<IBookService, BookService>();
+builder.Services.AddScoped<MySqlConnection>(_ => new MySqlConnection(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
